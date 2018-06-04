c8daff23054c19565aa2716476dfc0cf46a095c2
Janna Gertz <janna.lynn@web.de>
2018-06-04 15:33:50 +0200

    Add ToggleButton

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+
+import ToggleButton from '../src/components/ToggleButton'
+
+storiesOf('ToggleButton', module)
+  .add('default', () => (
+    <ToggleButton text={'Essen mitnehmen'} checked={false} onClick={'click'} />
+  ))
+  .add('checked', () => (
+    <ToggleButton text={'Essen mitnehmen'} checked={true} onClick={'click'} />
+  ))
