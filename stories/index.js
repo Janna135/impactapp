b6137d88324ef4f88f54d37bbeceb87a2261a6f4
Janna Gertz <janna.lynn@web.de>
2018-06-07 08:20:41 +0200

    Add SwitchButton to Components and Storybook

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions'
 import ToggleButton from '../src/components/ToggleButton'
 import CountButton from '../src/components/CountButton'
 import DateSelect from '../src/components/DateSelect'
+import SwitchButton from '../src/components/SwitchButton'
 
 storiesOf('ToggleButton', module)
   .add('default', () => (
@@ -21,3 +22,21 @@ storiesOf('CountButton', module).add('default', () => (
 storiesOf('DateSelect', module).add('default', () => (
   <DateSelect date="22.02.2018" onClick={'click'} />
 ))
+
+storiesOf('SwitchButton, module')
+  .add('left selected', () => (
+    <SwitchButton
+      selected={0}
+      onClick={action('click')}
+      textLeft={'All'}
+      textRight={'Days'}
+    />
+  ))
+  .add('right selected', () => (
+    <SwitchButton
+      selected={1}
+      onClick={action('click')}
+      textLeft={'All'}
+      textRight={'Days'}
+    />
+  ))
